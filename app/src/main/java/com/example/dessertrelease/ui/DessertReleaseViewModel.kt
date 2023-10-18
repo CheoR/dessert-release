e0094d93a03ffff3eda38a23b3f3d39661064d7d
CheoR <cheo.roman+github@gmail.com>
2023-10-18 11:52:59 -0500

    ref(ui): existing uiState ViewModel StateFlow to reflect repository isLinearLayout: Flow.

--- app/src/main/java/com/example/dessertrelease/ui/DessertReleaseViewModel.kt
+++ app/src/main/java/com/example/dessertrelease/ui/DessertReleaseViewModel.kt
@@ -9,8 +9,10 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.dessertrelease.DessertReleaseApplication
 import com.example.dessertrelease.R
 import com.example.dessertrelease.data.UserPreferencesRepository
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 /*
@@ -20,10 +22,23 @@ class DessertReleaseViewModel(
     private val userPreferencesRepository: UserPreferencesRepository
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow(DessertReleaseUiState())
-
     // UI states access for various [DessertReleaseUiState]
-    val uiState: StateFlow<DessertReleaseUiState> = _uiState
+    val uiState: StateFlow<DessertReleaseUiState> =
+        // Repository linear layout preference has two possible values, true or false, in form of
+        // Flow<Boolean>. This value must map to UI state.
+        // UserPreferencesRepository.isLinearLayout is cold Flow. Better to use hot flow to provide
+        // state to UI, like StateFlow, so state always available immediately to UI.
+        userPreferencesRepository.isLinearLayout.map { isLinearLayout ->
+            // Return DessertReleaseUiState instance data class instance, passing isLinearLayout
+            // Boolean. Screen uses this UI state to determine correct strings and icons to display.
+            DessertReleaseUiState(isLinearLayout)
+        }
+            // stateIn - convert Flow to StateFlow
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = DessertReleaseUiState()
+            )
 
     /*
      * [selectLayout] change layout and icons accordingly and
