85cb54d1244f4b939a1506f24c1f64f160f82e74
CheoR <cheo.roman+github@gmail.com>
2023-10-18 10:46:23 -0500

    build(ui): provide repository to viewModel through dependency injection.

--- app/src/main/java/com/example/dessertrelease/ui/DessertReleaseViewModel.kt
+++ app/src/main/java/com/example/dessertrelease/ui/DessertReleaseViewModel.kt
@@ -2,16 +2,21 @@ package com.example.dessertrelease.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.dessertrelease.DessertReleaseApplication
 import com.example.dessertrelease.R
+import com.example.dessertrelease.data.UserPreferencesRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 /*
  * View model of Dessert Release components
  */
-class DessertReleaseViewModel() : ViewModel() {
+class DessertReleaseViewModel(
+    private val userPreferencesRepository: UserPreferencesRepository
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow(DessertReleaseUiState())
 
@@ -29,7 +34,8 @@ class DessertReleaseViewModel() : ViewModel() {
     companion object {
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
-                DessertReleaseViewModel()
+                val application = (this[APPLICATION_KEY] as DessertReleaseApplication)
+                DessertReleaseViewModel(application.userPreferencesRepository)
             }
         }
     }
